309b6721978a78173769d332491e5685b2fc5a99
Chun Sreymech <chunsreymech92@gmail.com>
2026-07-30 23:01:02 +0700

    c

--- Controller/CategoryApiController.cs
+++ Controller/CategoryApiController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ExercisesLesson05.Data;
 
 namespace ExercisesLesson05.Controller
 {
@@ -10,6 +11,21 @@ namespace ExercisesLesson05.Controller
     [Route("api/[controller]")]
     public class CategoryApiController : ControllerBase
     {
-        
+        private readonly StoreDbContext _context;
+
+        // Constructor ទទួលយក DbContext តាមរយៈ DI
+        public CategoryApiController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        // Endpoint សម្រាប់ទាញទិន្នន័យទាំងអស់ជាទម្រង់ JSON
+        [HttpGet]
+        public IActionResult GetAllCategories()
+        {
+            // .ToList() នឹងបញ្ជាទៅ Database ឱ្យ Select ទិន្នន័យទាំងអស់ពី Table
+            var categories = _context.Categories.ToList();
+            return Ok(categories);
+        }
     }
 }
